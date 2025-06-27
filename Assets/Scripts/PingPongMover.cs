fdb5d30fa59f6d37f1d245206df1ed6a5a82b9ee
Leonid Barsht <leonid.barsht@htw-berlin.de>
2025-06-27 23:58:59 +0200

    Import audios, add animated hands to test scene

---
+++ Assets/Scripts/PingPongMover.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PingPongMover : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset = new Vector3(0, 2, 0);
+    [SerializeField] private float duration = 2f; 
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private float timer;
+
+    void Start()
+    {
+        startPos = transform.position;
+        endPos = startPos + offset;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float t = Mathf.PingPong(timer / duration, 1f);
+        transform.position = Vector3.Lerp(startPos, startPos + offset, t);
+    }
+}
